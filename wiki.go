9b79db9fd7c972456652e17ba14e8274835ed698
Antony Ndungu <antony.ndungu.maina@gmail.com>
2018-06-27 17:24:31 +0300

    Initial commit

---
+++ wiki.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"log"
+	"html/template"
+	"fmt"
+	"regexp"
+	"errors"
+)
+
+// Page as a struct with two fields representing the title and body
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func (p *Page) String() string {
+	return fmt.Sprintf("Title: %s \nBody:\n%s", p.Title, p.Body)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title, err := getTitle(w, r)	
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title, err := getTitle(w, r)	
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request){
+	title, err := getTitle(w, r)	
+	if err != nil {
+		return
+	}
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w,r, "/view/"+ title, http.StatusFound)
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request)(string, error){
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
+
+
